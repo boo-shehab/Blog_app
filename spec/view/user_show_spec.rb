22a0907619e74e667627cc89fe4c34f1f1a8a193
ahmed-bushhab <bfhetyrhyr12@gmail.com>
2023-03-21 13:45:39 +0300

    fix testing

--- spec/view/user_show_spec.rb
+++ spec/view/user_show_spec.rb
@@ -2,26 +2,26 @@ require 'rails_helper'
 
 RSpec.describe 'test user/show', type: :feature do
   before(:all) do
-    @user = User.find(340)
+    @user = User.find(2)
   end
   scenario 'users photo test' do
-    visit user_path(340)
+    visit user_path(2)
     expect(page).to have_xpath("//img[@src='#{@user.photo}']")
   end
   scenario 'users name test' do
-    visit user_path(340)
+    visit user_path(2)
     expect(page).to have_content(@user.name)
   end
   scenario 'users post_counter test' do
-    visit user_path(340)
+    visit user_path(2)
     expect(page).to have_content("Number of posts : #{@user.post_counter}")
   end
   scenario 'users bio test' do
-    visit user_path(340)
+    visit user_path(2)
     expect(page).to have_content(@user.bio)
   end
   scenario 'users post test' do
-    visit user_path(340)
+    visit user_path(2)
     @user.post.take(3).each do |post|
       expect(page).to have_content(post.title)
       expect(page).to have_content(post.text)
@@ -30,17 +30,17 @@ RSpec.describe 'test user/show', type: :feature do
     end
   end
   scenario 'users chack link test' do
-    visit user_path(340)
+    visit user_path(2)
     expect(page).to have_link('See all posts', href: user_post_index_path(user_id: @user.id))
   end
   scenario 'users click see all posts test' do
-    visit user_path(340)
+    visit user_path(2)
     click_on('See all posts')
     expect(page).to have_current_path(user_post_index_path(user_id: @user.id))
   end
 
   scenario 'users click see all posts test' do
-    visit user_path(340)
+    visit user_path(2)
     first_post = @user.post.take(3)[0]
     click_link(href: "/user/#{@user.id}/post/#{first_post.id}")
 
